5cab0eee3e0fa7ab3793ab60725663df9f05ebc0
CapellNathanael <nathanael.capell@etu.umontpellier.fr>
2019-12-16 16:24:46 +0100

    modif routeur obligation de se connecter pour accéder au site

--- controller/routeur.php
+++ controller/routeur.php
@@ -8,26 +8,37 @@ require_once File::build_path(array("controller", "ControllerVisualisation.php")
 
 $controller_default = "utilisateur";
 
-if (isset($_GET['controller']) == true && isset($_GET['action']) == true) {
-	$controller = $_GET['controller'];
-	$controller_class = 'Controller' . ucfirst($controller);
-	if (!class_exists($controller_class)) {
-		$controller_class = 'ControllerFormulaire';
-		$action = 'error';
-	}
-	else {
-		$methodsController = get_class_methods($controller_class);
-		if (in_array($_GET['action'], $methodsController)) {
-			$action = $_GET['action'];
-		}
-		else {
+if (isset($_SESSION["Identifiant"])){
+	if (isset($_GET['controller']) == true && isset($_GET['action']) == true) {
+		$controller = $_GET['controller'];
+		$controller_class = 'Controller' . ucfirst($controller);
+		if (!class_exists($controller_class)) {
+			$controller_class = 'ControllerFormulaire';
 			$action = 'error';
 		}
+		else {
+			$methodsController = get_class_methods($controller_class);
+			if (in_array($_GET['action'], $methodsController)) {
+				$action = $_GET['action'];
+			}
+			else {
+				$action = 'error';
+			}
+		}
 	}
-}
-else {
+	else {
+		$controller_class = 'Controller' . ucfirst($controller_default);
+		$action = 'connect';
+	}
+} else if (isset($_GET['controller']) == true && isset($_GET['action']) == true && $_GET['controller'] == 'utilisateur' && $_GET['action'] == 'connected'){
+	$controller = $_GET['controller'];
+	$controller_class = 'Controller' . ucfirst($controller);
+	$action = $_GET['action'];
+
+} else{
 	$controller_class = 'Controller' . ucfirst($controller_default);
 	$action = 'connect';
+
 }
 // Appel de la méthode statique $action de ControllerVoiture
 $controller_class::$action();
